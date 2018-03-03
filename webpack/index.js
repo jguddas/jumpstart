64ca2f845728bf94873762a5f8b2b269cf8191d4
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-03-03 16:56:19 +0100

    added content base template

--- webpack/index.js
+++ webpack/index.js
@@ -1,15 +1,22 @@
 const path = require('path')
 const LogPlugin = require('./plugins/log-plugin')
 
-module.exports = (env, { mode }) => {
+module.exports = (env, { mode, contentBase }) => {
   const production = (env || mode) === 'production'
 
   const plugins = [
     new LogPlugin(() => production && process.stderr.clearLine())
   ]
   const rules = []
+  const devServer = {
+    contentBase: [
+      contentBase || './',
+      path.join(__dirname, 'template'),
+    ]
+  }
 
   return {
+    devServer,
     plugins,
     module: { rules },
   }
