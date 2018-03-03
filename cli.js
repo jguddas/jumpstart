4c3a174170d96deb900db1d8e965a1cb09272622
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-03-03 12:53:31 +0100

    added webpack dev server to the cli script

--- cli.js
+++ cli.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process')
-spawn(
-  require.resolve('.bin/webpack-cli'),
-  process.argv.slice(2).concat(['--config', require.resolve('./webpack')]),
-  { stdio: 'inherit' }
-)
+if (process.argv[2] === 'webpack-cli') {
+  spawn(
+    require.resolve('.bin/webpack-cli'),
+    process.argv.slice(3).concat(['--config', require.resolve('./webpack')]),
+    { stdio: 'inherit' }
+  )
+} else if (process.argv[2] === 'webpack-dev-server') {
+  spawn(
+    require.resolve('.bin/webpack-dev-server'),
+    process.argv.slice(3).concat(['--config', require.resolve('./webpack')]),
+    { stdio: 'inherit' }
+  )
+}
