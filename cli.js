7e240ec8cd7b90fc6b3c7017e90d7942832f6b7e
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-03-05 16:54:04 +0100

    simplified cli script

--- cli.js
+++ cli.js
@@ -3,23 +3,31 @@ const { spawn } = require('child_process')
 const dargs = require('dargs')
 const minimist = require('minimist')
 
-const assign = (a, b) => Object.assign(a, b)
-const concat = x => [].concat(x || []).reduce(assign, {})
 const bin = command => require.resolve(`.bin/${command}`)
-const run = (command, args, options, dargOpts) =>
-  spawn(bin(command), dargs(concat(args), concat(dargOpts)), concat(options))
-
-const processOpts = { stdio: 'inherit' }
-const webpackOpts = { config: require.resolve('./webpack') }
-const webpackCliOpts = { mode: 'production' }
-const webpackServerOpts = { mode: 'development', quiet: true }
+const run = (command, args, options) =>
+  spawn(bin(command), dargs(args), options)
 
 const argv = minimist(process.argv.slice(3))
 
+const processOpts = { stdio: 'inherit' }
+const webpackCliOpts = {
+  mode: 'production',
+  ...argv,
+  config: require.resolve('./webpack'),
+  progress: false,
+}
+const webpackServerOpts = {
+  quiet: true,
+  mode: 'development',
+  ...argv,
+  config: require.resolve('./webpack'),
+  progress: false,
+}
+
 if (process.argv[2] === 'webpack-cli') {
-  run('webpack-cli', [webpackOpts, webpackCliOpts, argv], processOpts)
+  run('webpack-cli', webpackCliOpts, processOpts)
 } else if (process.argv[2] === 'webpack-dev-server') {
-  run('webpack-dev-server', [webpackOpts, webpackServerOpts, argv], processOpts)
+  run('webpack-dev-server', webpackServerOpts, processOpts)
 } else {
   console.log('Invalid command!')
   console.log('$ jumpstart webpack-cli|webpack-dev-server [options]')
