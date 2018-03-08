7d3160b4e70bcf137deb8484ad6c153914d4520b
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-03-08 10:34:10 +0100

    move default babel options into babel loader function

--- webpack/rules/babel.js
+++ webpack/rules/babel.js
@@ -1,21 +1,26 @@
-const babelLoader = (presets = [], plugins = []) => ({
-  loader: require.resolve('babel-loader'),
-  options: { presets, plugins }
-})
-module.exports = ({ pragma }) => [{
-  test: /\.js$/,
-  exclude: /node_modules/,
-  use: babelLoader([[
-    require.resolve('babel-preset-env'),
-    { modules: false, loose: true }
-  ]], [
-    require.resolve('babel-plugin-transform-class-properties'),
-    [
-      require.resolve('babel-plugin-transform-object-rest-spread'),
-      { useBuiltIns: true },
-    ], [
-      require.resolve('babel-plugin-transform-react-jsx'),
-      { useBuiltIns: true, pragma },
-    ]
-  ])
-}]
+module.exports = ({ pragma }) => {
+  const babelLoader = (presets, plugins, override) => ({
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: (override && presets) || [[
+        require.resolve('babel-preset-env'),
+        { modules: false, loose: true }
+      ]].concat(presets || []),
+      plugins: (override && plugins) || [
+        require.resolve('babel-plugin-transform-class-properties'),
+        [
+          require.resolve('babel-plugin-transform-object-rest-spread'),
+          { useBuiltIns: true },
+        ], [
+          require.resolve('babel-plugin-transform-react-jsx'),
+          { useBuiltIns: true, pragma },
+        ]
+      ].concat(plugins || []),
+    }
+  })
+  return [{
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: babelLoader()
+  }]
+}
