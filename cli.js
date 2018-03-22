4509771b5ab37bd82b559db3a6f15d88f357c385
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-03-22 15:19:56 +0100

    changed cli to use oargs

--- cli.js
+++ cli.js
@@ -1,56 +1,43 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process')
 const dargs = require('dargs')
-const minimist = require('minimist')
+const oargs = require('oargs')
 
-const bin = command => require.resolve(`.bin/${command}`)
-const run = (command, args, options) =>
-  spawn(bin(command), dargs(args), options)
-
-const argv = minimist(process.argv.slice(3))
-
-const webpackCliOpts = {
-  stdio: 'inherit',
-  env: {
-    ...process.env,
-    JUMPSTART: JSON.stringify({
-      progress: true,
-      ...argv,
-    }),
+const run = command => out => spawn(
+  require.resolve(`.bin/${command}`),
+  dargs({ ...out.mapped.default, _: out.argv._ }),
+  {
+    stdio: 'inherit',
+    env: {
+      ...process.env,
+      JUMPSTART: JSON.stringify(out.mapped.env),
+    }
   }
-}
-const webpackServerOpts = {
-  stdio: 'inherit',
-  env: {
-    ...process.env,
-    JUMPSTART: JSON.stringify({
-      progress: true,
-      ...argv,
-    }),
-  }
-}
-const webpackCliArgs = {
-  mode: 'production',
-  ...argv,
-  config: require.resolve('./webpack'),
-  pragma: undefined,
-  progress: false,
-}
-const webpackServerArgs = {
-  quiet: argv.progress !== false,
-  mode: 'development',
-  port: 3000,
-  ...argv,
-  config: require.resolve('./webpack'),
-  pragma: undefined,
-  progress: false,
-}
+)
 
-if (process.argv[2] === 'webpack-cli') {
-  run('webpack-cli', webpackCliArgs, webpackCliOpts)
-} else if (process.argv[2] === 'webpack-dev-server') {
-  run('webpack-dev-server', webpackServerArgs, webpackServerOpts)
-} else {
-  if(process.argv[2]) console.log('Invalid command!')
-  console.log('$ jumpstart webpack-cli|webpack-dev-server [options]')
-}
+const cli = oargs()
+
+cli
+  .command('build', {
+    description: 'run webpack-cli',
+    filter: ['default']
+  }, run('webpack-cli'))
+  .option('mode', { default: 'production', inHelp: false })
+  .option('pragma', { filter: 'env', description: 'set jsx pragma' })
+  .option('progress', { default: true, filter: 'env', inHelp: false })
+  .option('config', { overide: require.resolve('./webpack'), inHelp: false })
+  .option('help', { description: 'show webpack-cli help' })
+
+cli
+  .command('start', {
+    description: 'run webpack-dev-server'
+  }, run('webpack-dev-server'))
+  .option('mode', { default: 'development', inHelp: false})
+  .option('port', { default: 3000, inHelp: false })
+  .option('pragma', { filter: 'env', description: 'set jsx pragma' })
+  .option('config', { overide: require.resolve('./webpack'), inHelp: false })
+  .option('progress', { default: true, filter: 'env', inHelp: false })
+  .option('quiet', { default: true, inHelp: false })
+  .option('help', { description: 'show webpack-dev-server help' })
+
+if (!cli.parse()) cli.showHelp(require('./package.json'))
