1484ded746f08fd3e29bb177f61cb5cb3e40a006
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-04-02 18:10:29 +0200

    added copy-webpack-plugin

--- webpack/index.js
+++ webpack/index.js
@@ -3,16 +3,21 @@ const styleRules = require('./rules/style')
 const babelRules = require('./rules/babel')
 const LogPlugin = require('./plugins/log-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = (env, { mode, contentBase }) => {
+module.exports = (env, { mode, outputPublicPath }) => {
   const argv = JSON.parse(process.env.JUMPSTART || '{}')
   const production = (env || mode) === 'production'
 
+  const contentBase = [
+    path.join(__dirname, 'template')
+  ].concat(outputPublicPath || []).reverse()
   const plugins = [
     new ExtractTextPlugin({
       filename: 'style.css',
       disable: !production,
     }),
+    new CopyWebpackPlugin(contentBase)
   ].concat(!argv.progress ? [] :
     new LogPlugin(() => production && process.stderr.clearLine())
   )
@@ -20,12 +25,7 @@ module.exports = (env, { mode, contentBase }) => {
     ...babelRules({ pragma: argv.pragma }),
     ...styleRules(ExtractTextPlugin.extract, { minimize: production }),
   ]
-  const devServer = {
-    contentBase: [
-      contentBase || './',
-      path.join(__dirname, 'template'),
-    ]
-  }
+  const devServer = { contentBase }
   const extensions = ['.js', '.jsx', '.lsc', '.lsx']
   const devtool = !production && 'cheap-source-map'
 
