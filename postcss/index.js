fc6ad999e0f90ae67f40e152e8447e3e8c3c62d2
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-04-12 13:14:39 +0200

    added less and sass support for postcss-cli

--- postcss/index.js
+++ postcss/index.js
@@ -1,6 +1,33 @@
-module.exports = ({ options }) => {
+const postcss = require('postcss')
+const path = require('path')
+const sass = require('node-sass')
+const less = require('less')
+
+less.renderSync = function (input, options = {}) {
+  options.sync = true
+  let result
+  this.render(input, options, (err, val) => {
+    if (err) throw err
+    result = val
+  })
+  return result
+}
+
+module.exports = ({ file, options }) => {
+  const filename = path.join(file.dirname, file.basename)
+  let parser
+  if (/^\.s[ca]ss$/.test(file.extname)) {
+    parser = () => postcss.parse(
+      sass.renderSync({ file: filename }).css
+    )
+  } else if ('.less' === file.extname) {
+    parser = data => postcss.parse(
+      less.renderSync(String(data)).css
+    )
+  }
   return {
     ...options,
+    parser,
     plugins: [
       require('autoprefixer')(),
       require('postcss-short')(),
