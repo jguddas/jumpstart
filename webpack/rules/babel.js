52f4ed7917b4bda14e6cadf90d5b5f283dca2d97
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-09-07 20:04:46 +0200

    impoved webpack rules

--- webpack/rules/babel.js
+++ webpack/rules/babel.js
@@ -1,23 +1,40 @@
-const jsLoader = opts => ({
+const preset = require.resolve('../../babel')
+
+const babelLoader = opts => ({
   loader: require.resolve('babel-loader'),
-  options: { presets: require.resolve('../../babel'), ...opts },
+  options: opts,
 })
-module.exports = () => {
-  return [{
-    test: /\.jsx?$/,
-    oneOf: [
-      {
-        resourceQuery: /include/,
-        use: jsLoader(),
-      }, {
-        exclude: /node_modules/,
-        use: jsLoader(),
-      },
-    ],
-  }, {
-    test: /\.ls[cx]?$/,
-    use: jsLoader({
-      plugins: require.resolve('@oigroup/babel-plugin-lightscript'),
-    }),
-  }]
-}
+
+const oneOf = opts => opts ? [
+  { // include = preset
+    resourceQuery: /(^\?|&)include($|&)/,
+    use: babelLoader({ ...opts, presets: [preset].concat(opts.presets || []) }),
+  }, { // exclude = no preset
+    resourceQuery: /(^\?|&)exclude($|&)/,
+    use: babelLoader(opts),
+  }, { // not node_modules = preset
+    exclude: /node_modules/,
+    use: babelLoader({ ...opts, presets: [preset].concat(opts.presets || []) }),
+  }, { // node_modules = no preset
+    use: babelLoader(opts),
+  },
+] : [
+  { // include = preset
+    resourceQuery: /(^\?|&)include($|&)/,
+    use: babelLoader({ presets: [preset] }),
+  }, { // not node_modules & not exclude = preset
+    exclude: /node_modules/,
+    resourceQuery: { not: [/(^\?|&)exclude($|&)/] },
+    use: babelLoader({ presets: [preset] }),
+  },
+]
+
+module.exports = () => [{
+  test: /\.jsx?$/,
+  oneOf: oneOf(),
+}, {
+  test: /\.ls[cx]?$/,
+  oneOf: oneOf({
+    plugins: require.resolve('@oigroup/babel-plugin-lightscript'),
+  }),
+}]
--- webpack/rules/style.js
+++ webpack/rules/style.js
@@ -1,6 +1,18 @@
 const path = require('path')
+const sassLoader = require.resolve('sass-loader')
+const lessLoader = require.resolve('less-loader')
+
 const argv = JSON.parse(process.env.JUMPSTART || '{}')
-const cssLoader = (extract, opts = {}, after = []) => extract({
+const postLoader = [].concat(argv['css-plugins'] === false ? [] : {
+  loader: require.resolve('postcss-loader'),
+  options: {
+    config: {
+      path: path.dirname(require.resolve('../../postcss')),
+    },
+  },
+})
+
+const cssLoader = (extract, opts, after) => extract({
   fallback: require.resolve('style-loader'),
   use: [{
     loader: require.resolve('css-loader'),
@@ -9,47 +21,47 @@ const cssLoader = (extract, opts = {}, after = []) => extract({
       localIdentName: opts.modules && '[local]-[hash:base64:5]',
       ...opts,
     },
-  }].concat(argv['css-plugins'] === false ? [] : {
-    loader: require.resolve('postcss-loader'),
-    options: {
-      config: {
-        path: path.dirname(require.resolve('../../postcss')),
-      },
-    },
-  }).concat(after),
+  }].concat(after),
 })
-const sassLoader = (extract, opts = {}, after = []) =>
-  cssLoader(extract, opts, [require.resolve('sass-loader')].concat(after))
-const lessLoader = (extract, opts = {}, after = []) =>
-  cssLoader(extract, opts, [require.resolve('less-loader')].concat(after))
+
+const oneOf = (extract, opts, after = []) => [
+  { // include & modules = postcss & modules
+    resourceQuery: { and: [
+      /(^\?|&)modules($|&)/,
+      /(^\?|&)include($|&)/,
+    ] },
+    use: cssLoader(extract, { ...opts, modules: true }, postLoader.concat(after)),
+  }, { // exclude & modules = css & modules
+    resourceQuery: { and: [
+      /(^\?|&)modules($|&)/,
+      /(^\?|&)exclude($|&)/,
+    ] },
+    use: cssLoader(extract, { ...opts, modules: true }, after),
+  }, { // exclude = css
+    resourceQuery: /(^\?|&)exclude($|&)/,
+    use: cssLoader(extract, opts, after),
+  }, { // not node_modules & modules = postcss & modules
+    exclude: /node_modules/,
+    resourceQuery: /(^\?|&)modules($|&)/,
+    use: cssLoader(extract, { ...opts, modules: true }, postLoader.concat(after)),
+  }, { // node_modules & modules = css & modules
+    resourceQuery: /(^\?|&)modules($|&)/,
+    use: cssLoader(extract, { ...opts, modules: true }, after),
+  }, { // not node_modules = postcss
+    exclude: /node_modules/,
+    use: cssLoader(extract, opts, postLoader.concat(after)),
+  }, { // node_modules = css
+    use: cssLoader(extract, opts, after),
+  },
+]
+
 module.exports = (extract, opts = {}) => [{
-    test: /\.css$/,
-    oneOf: [
-      {
-        resourceQuery: /modules/,
-        use: cssLoader(extract, { ...opts, modules: true }),
-      }, {
-        use: cssLoader(extract, opts),
-      },
-    ],
-  }, {
-    test: /\.s[ac]ss$/,
-    oneOf: [
-      {
-        resourceQuery: /modules/,
-        use: sassLoader(extract, { ...opts, modules: true }),
-      }, {
-        use: sassLoader(extract, opts),
-      },
-    ],
-  }, {
-    test: /\.less$/,
-    oneOf: [
-      {
-        resourceQuery: /modules/,
-        use: lessLoader(extract, { ...opts, modules: true }),
-      }, {
-        use: lessLoader(extract, opts),
-      },
-    ],
-  }]
+  test: /\.css$/,
+  oneOf: oneOf(extract, opts),
+}, {
+  test: /\.s[ac]ss$/,
+  oneOf: oneOf(extract, opts, sassLoader),
+}, {
+  test: /\.less$/,
+  oneOf: oneOf(extract, opts, lessLoader),
+}]
