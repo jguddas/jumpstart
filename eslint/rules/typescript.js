5ab825029a5b0de882a85f7b2126bedc9cbe08f0
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-10-20 18:47:17 +0200

    added eslint typescript rules

--- eslint/rules/typescript.js
+++ eslint/rules/typescript.js
@@ -1,3 +1,23 @@
 module.exports = {
-  'no-undef': 'off'
+  'no-undef': 'off',
+  'typescript/adjacent-overload-signatures': 'error',
+  'typescript/class-name-casing': 'error',
+  'typescript/explicit-function-return-type': [
+    'error',
+    { allowExpressions: true },
+  ],
+  'typescript/member-delimiter-style': 'error',
+  'typescript/member-ordering': 'error',
+  'typescript/no-angle-bracket-type-assertion': 'error',
+  'typescript/no-array-constructor': 'error',
+  'typescript/no-empty-interface': 'error',
+  'typescript/no-inferrable-types': 'error',
+  'typescript/no-namespace': 'error',
+  'typescript/no-non-null-assertion': 'error',
+  'typescript/no-triple-slash-reference': 'error',
+  'typescript/no-unused-vars': 'error',
+  'typescript/no-use-before-define': 'error',
+  'typescript/no-var-requires': 'error',
+  'typescript/prefer-namespace-keyword': 'error',
+  'typescript/type-annotation-spacing': 'error',
 }
