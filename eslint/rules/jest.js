1d0238dc7f43ae404a8b4c1b4f3f35c561765e74
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-10-22 11:47:02 +0200

    added eslint jest rules

--- eslint/rules/jest.js
+++ eslint/rules/jest.js
@@ -1,3 +1,8 @@
 module.exports = {
   'no-undef': 'off',
+  'jest/expect-expect': 'error',
+  'jest/no-disabled-tests': 'warn',
+  'jest/no-focused-tests': 'error',
+  'jest/no-identical-title': 'error',
+  'jest/valid-expect': 'error',
 }
