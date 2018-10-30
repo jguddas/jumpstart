27137a2164a4ee28a4ae82ef315a0efa6b09e716
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-10-30 11:37:35 +0100

    removed addOpts babelrc helper

--- babel/index.js
+++ babel/index.js
@@ -1,8 +1,5 @@
 const resolve = require('resolve')
 
-const addOpts = (...x) => (...opts) => x
-  .map(val => [require.resolve(val), Object.assign({}, ...opts)])
-
 const resolvePlugins = plugins => plugins.map(val => {
   if (typeof val === 'object') {
     return [resolve.sync(val[0], { basedir: process.cwd() }), val[1]]
@@ -13,13 +10,15 @@ const resolvePlugins = plugins => plugins.map(val => {
 const argv = JSON.parse(process.env.JUMPSTART || '{}')
 module.exports = (context, opts = {}) => {
   return {
-      presets: argv['presets'] === false ? [] : addOpts('@babel/preset-env')({
-      modules: false,
-      loose: true,
-    }, opts, {
-      modules: argv['modules'] || opts.modules || false,
-      targets: argv['env-target'] || opts.targets || {},
-    }).concat(
+    presets: (argv['presets'] === false ? [] : [[
+      require.resolve('@babel/preset-env'),
+      {
+        loose: true,
+        ...opts,
+        modules: argv['modules'] || opts.modules || false,
+        targets: argv['env-target'] || opts.targets || {},
+      }
+    ]]).concat(
       (argv['presets'] || []).map(val => {
         if (typeof val === 'object') {
           return [resolve.sync(val[0], { basedir: process.cwd() }), val[1]]
@@ -28,22 +27,26 @@ module.exports = (context, opts = {}) => {
       }),
     ),
     // if argv.plugins.0 is false do not load default plugins
-    plugins: (argv['plugins'] || [])[0] === false ? resolvePlugins(
+    plugins: ((argv['plugins'] || [])[0] === false ? resolvePlugins(
       argv['plugins'].slice(1)
-    ) : addOpts(
-      '@babel/plugin-proposal-decorators',
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-transform-react-jsx',
-      '@babel/plugin-syntax-dynamic-import',
-    )({
-      useBuiltIns: true,
-      pragma: argv['pragma'],
-      legacy: argv['decorator-legacy'],
-    },
-      argv['decorator-legacy'] ? {} : {
-        decoratorsBeforeExport: true,
-      }
-    , opts).concat(
+    ) : [
+      [
+        require.resolve('@babel/plugin-proposal-decorators'),
+        {
+          legacy: argv['decorator-legacy'],
+          ...(argv['decorator-legacy'] ? {} : {
+            decoratorsBeforeExport: !argv['decorator-legacy'],
+          })
+        },
+      ], [
+        require.resolve('@babel/plugin-proposal-class-properties'),
+      ], [
+        require.resolve('@babel/plugin-transform-react-jsx'),
+        { pragma: argv['pragma'] }
+      ], [
+        require.resolve('@babel/plugin-syntax-dynamic-import'),
+      ]
+    ]).concat(
       resolvePlugins(argv['plugins'] || [])
     ),
   }
