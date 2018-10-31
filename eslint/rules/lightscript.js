af576812eb0096fc578aab22cc58f2bc0ac31694
Jakob Guddas <jakob.guddas@openmailbox.org>
2018-10-31 18:32:54 +0100

    added eslint lightscript rules

--- eslint/rules/lightscript.js
+++ eslint/rules/lightscript.js
@@ -2,4 +2,13 @@ const lscConfigs = require('@lightscript/eslint-plugin/lib/configs')
 module.exports = {
   ...lscConfigs.recommended.rules,
   'space-infix-ops': 'off',
+  'arrow-parens': ['error', 'as-needed'],
+  'arrow-spacing': 'off',
+  'comma-dangle': 'off',
+  'constructor-super': 'off',
+  'no-this-before-super': 'off',
+  'brace-style': 'off',
+  'function-paren-newline': 'off',
+  'space-before-blocks': 'off',
+  'react/jsx-indent': 'off',
 }
