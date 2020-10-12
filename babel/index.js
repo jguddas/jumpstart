66646d190898ba11269708a065330def4f0daf49
Jakob Guddas <jakob.guddas@openmailbox.org>
2020-10-12 18:07:19 +0200

    enabled babel config caching

--- babel/index.js
+++ babel/index.js
@@ -8,43 +8,50 @@ const resolvePlugins = plugins => plugins.filter(Boolean).map(val => {
 })
 
 const argv = JSON.parse(process.env.JUMPSTART || '{}')
-module.exports = (context, opts = {}) => ({
+module.exports = (context, opts = {}) => {
+  context.cache.using(() => JSON.stringify([
+    opts,
+    process.cwd(),
+    process.env.JUMPSTART,
+  ]))
+  return {
   // if argv.presets.0 is false do not load default presets
-  presets: ((argv.presets || [])[0] === false ? [] : [[
-    require.resolve('@babel/preset-env'),
-    {
-      loose: true,
-      ...opts,
-      modules: argv.modules || opts.modules || false,
-      targets: argv['env-target'] || opts.targets || {},
-    },
-  ]]).concat(resolvePlugins(argv.presets || [])),
-  // if argv.plugins.0 is false do not load default plugins
-  plugins: ((argv.plugins || [])[0] === false ? [] : [
-    [
-      require.resolve('@babel/plugin-proposal-decorators'),
+    presets: ((argv.presets || [])[0] === false ? [] : [[
+      require.resolve('@babel/preset-env'),
       {
-        legacy: argv['decorator-legacy'],
-        ...(argv['decorator-legacy'] ? {} : {
-          decoratorsBeforeExport: !argv['decorator-legacy'],
-        }),
+        loose: true,
+        ...opts,
+        modules: argv.modules || opts.modules || false,
+        targets: argv['env-target'] || opts.targets || {},
       },
-    ], [
-      require.resolve('@babel/plugin-proposal-class-properties'),
-    ], [
-      require.resolve('@babel/plugin-transform-react-jsx'),
-      { pragma: argv.pragma },
-    ], [
-      require.resolve('@babel/plugin-syntax-dynamic-import'),
-    ], [
-      require.resolve('@babel/plugin-proposal-optional-chaining'),
-    ], [
-      require.resolve('@babel/plugin-proposal-throw-expressions'),
-    ], [
-      require.resolve('@babel/plugin-proposal-pipeline-operator'),
-      { proposal: 'minimal' },
-    ], [
-      require.resolve('babel-plugin-macros'),
-    ],
-  ]).concat(resolvePlugins(argv.plugins || [])),
-})
+    ]]).concat(resolvePlugins(argv.presets || [])),
+    // if argv.plugins.0 is false do not load default plugins
+    plugins: ((argv.plugins || [])[0] === false ? [] : [
+      [
+        require.resolve('@babel/plugin-proposal-decorators'),
+        {
+          legacy: argv['decorator-legacy'],
+          ...(argv['decorator-legacy'] ? {} : {
+            decoratorsBeforeExport: !argv['decorator-legacy'],
+          }),
+        },
+      ], [
+        require.resolve('@babel/plugin-proposal-class-properties'),
+      ], [
+        require.resolve('@babel/plugin-transform-react-jsx'),
+        { pragma: argv.pragma },
+      ], [
+        require.resolve('@babel/plugin-syntax-dynamic-import'),
+      ], [
+        require.resolve('@babel/plugin-proposal-optional-chaining'),
+      ], [
+        require.resolve('@babel/plugin-proposal-throw-expressions'),
+      ], [
+        require.resolve('@babel/plugin-proposal-pipeline-operator'),
+        { proposal: 'minimal' },
+      ], [
+        require.resolve('babel-plugin-macros'),
+      ],
+    ]).concat(resolvePlugins(argv.plugins || [])),
+  }
+}
